f992cc2c4fa19a4ee88a0a5e00caa66661756140
Paul West <fp8049@gmail.com>
2020-03-14 15:51:48 -0400

    Added sampler function

--- R/sampler.R
+++ R/sampler.R
@@ -2,51 +2,71 @@
 #'
 #' \code{sampler} generates Simple Random or Stratified samples
 #'
-#'@return Writes samples to an Excel workbook
-#'@export
+#' @return Writes samples to an Excel workbook
+#' @importFrom magrittr "%>%"
+#' @importFrom tools file_ext
+#' @importFrom purrr map2_dfr
+#' @import openxlsx
+#' @importFrom data.table fread
+#' @import dplyr
+#'
+#' @export
+#'
+sampler <- function() {
+  set.seed(12345)
+  wb <- createWorkbook()
 
-sampler <- function()
+  dataName <- file.choose()
 
-dataName <- file.choose()
-
-if (file_ext(dataName)=='xlsx') {
-  data <- read.xlsx(dataName)
-  } else if (file_ext(dataName) == 'csv') {
+  if(file_ext(dataName)=='xlsx') {
+    data <- read.xlsx(dataName)
+  } else if(file_ext(dataName)=='csv') {
     data <- fread(dataName)
-  } else print("Not a valid data file")
+  } else paste("Not a valid data file")
 
-sample.size <- ssize(nrow(data))
+  sampleSize <- whSample::ssize(nrow(data))
 
-set.seed(12345)       # Allows for replication; otherwise not necessary
+  sampleType <- utils::menu(c("Simple Random Sample",
+                              "Stratified Random Sample",
+                              "Tabbed Stratified Sample"),
+                            graphics=T,
+                            title="Sample Type")
 
-print(sample.size)
+  if(sampleType == 1L) {
+    addWorksheet(wb, "Simple Random Sample")
+    writeData(wb, "Simple Random Sample", data[sample(nrow(data)),])
+    saveWorkbook(wb, file="Samples.xlsx", overwrite=T)
 
-# wb <- createWorkbook()
-#
-# addWorksheet(wb,"Simple Random Sample")
-# writeData(wb,"Simple Random Sample", data[sample(nrow(data)),])
-#
-# saveWorkbook(wb, file = "Samples.xlsx", overwrite = TRUE)
-#
-# data.samples <-
-#   data %>% count(Function) %>%
-#   mutate(prop=prop.table(n)) %>%
-#   mutate(numSamples=ceiling(
-#     ifelse(prop*sample.size<1, 1, prop*sample.size)))
-#
-# data.list <- split(data,data$Function)
-#
-# data3 <- map2_dfr(data.list, data.samples$numSamples,
-#                              ~sample_n(.x, .y))
-# data3 %>% count(Function)
-#
-# addWorksheet(wb,"Random by Function")
-# writeData(wb,"Random by Function", data3)
-# data.split <- split(data3,data3$Function)
-#
-# Map(function(data, name){
-#   addWorksheet(wb, name)
-#   writeData(wb, name, data)
-# }, data.split, names(data.split))
-#
-# saveWorkbook(wb, file = "Samples.xlsx", overwrite = TRUE)
+  } else {
+
+    stratifyOn <- names(data)[utils::menu(names(data), graphics=T,
+                                          title="Stratify on")]
+
+    dataSamples <- data %>% group_by_at(stratifyOn) %>% count() %>%
+      data.table::setDT() %>% mutate(prop = prop.table(n)) %>%
+      mutate(numSamples = ceiling(ifelse(
+        prop * sampleSize < 1, 1, prop * sampleSize
+      )))
+
+    data.table::setDF(data)
+    dataList <- split(data,data[stratifyOn])
+
+    data2 <- map2_dfr(dataList, dataSamples$numSamples,
+                      ~sample_n(.x, .y))
+
+    if(sampleType == 2L){
+      addWorksheet(wb,"Stratified Random Sample")
+      writeData(wb,"Stratified Random Sample", data2)
+
+    } else if(sampleType == 3L){
+      dataTabs <- split(data2, data2[stratifyOn])
+
+      Map(function(data, name){
+        addWorksheet(wb, name)
+        writeData(wb, name, data)
+      }, dataTabs, names(dataTabs))
+
+    }
+    saveWorkbook(wb, file = "Samples.xlsx", overwrite=T)
+  }
+}
